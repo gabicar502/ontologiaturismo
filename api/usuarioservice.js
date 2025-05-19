68093b963a42fe1e889e83aabebfcef5376c0b75
gabicar502 <ga.cardenas@udla.edu.co>
2025-05-19 13:07:32 -0500

    ip actu

--- api/usuarioservice.js
+++ api/usuarioservice.js
@@ -5,7 +5,7 @@ class UsuarioService {
     constructor() {
       this.pool = new Pool({
         user: 'verceluser',               // o postgres, según creaste el usuario
-        host: '3.148.235.97',            // ← IP pública de tu instancia EC2
+        host: '18.118.152.189',            // ← IP pública de tu instancia EC2
         database: 'turismo',              // ← Tu base de datos real
         password: 'vercel123',            // ← La contraseña que diste
         port: 5432,
